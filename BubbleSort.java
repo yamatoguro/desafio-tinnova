116039635c3078c09250423471a1204a168423dc
Iago Reis <yamatoguro@gmail.com>
2021-07-17 16:02:28 -0300

    Implementação do BubbleSort

---
+++ BubbleSort.java
@@ -0,0 +1,14 @@
+public class BubbleSort {
+    public static void ordenar(int[] lista, int tam) {
+        if (tam == 1)
+            return;
+        for (int i = 0; i < tam - 1; i++) {
+            if (lista[i] > lista[i + 1]) {
+                int a = lista[i];
+                lista[i] = lista[i + 1];
+                lista[i + 1] = a;
+            }
+        }
+        ordenar(lista, --tam);
+    }
+}
